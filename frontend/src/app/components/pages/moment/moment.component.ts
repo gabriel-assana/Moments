75536393f56815c867c41d970e2c63749c3523a8
Gabriel Assana <gabrielhhz2@gmail.com>
2022-09-03 21:27:28 -0300

    Subindo alterações na pagina de detalhes dos moments

--- frontend/src/app/components/pages/moment/moment.component.ts
+++ frontend/src/app/components/pages/moment/moment.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { faEdit, faTimes } from '@fortawesome/free-solid-svg-icons';
 import { Moment } from 'src/app/Moment';
 import { MomentService } from 'src/app/services/moment.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-moment',
@@ -10,7 +12,11 @@ import { MomentService } from 'src/app/services/moment.service';
 })
 export class MomentComponent implements OnInit {
 
+  faTimes = faTimes;
+  faEdit = faEdit;
+
   moment?: Moment;
+  baseApiUrl = environment.baseApiUrl
 
   constructor(
     private momentService: MomentService, 
